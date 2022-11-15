0ccefe505389b09f5cd4fa2b62b03d1405bdfd78
Yuyang Lin <yuyang.lin22@ucl.ac.uk>
2022-11-15 22:54:24 +0000

    summary

--- cities.py
+++ cities.py
@@ -76,7 +76,7 @@ class CityCollection:
             count=0.0
             for index in self.cities:
                 if(index.country==list1[i]):
-                    count+=index.distance_to(city)
+                    count+=index.distance_to(city)*index.number
             list2.append(count)   
         dict_tbc = dict(zip(list1, list2))
         return dict_tbc
@@ -84,29 +84,30 @@ class CityCollection:
 
     def total_co2(self, city: City) -> float:
         tc=0
-        for index in range(1,len(self)):
-            tc+=self[index].co2_to(city)
+        for index in self.cities:
+            tc+=index.co2_to(city)*index.number
         return tc
         
 
     def co2_by_country(self, city: City) -> Dict[str, float]:
         list1=self.countries()
         list2=[]
-        list2[0]=self[1].co2_to(city)
-        for index in range(2,len(self)):
-            if(self[index].country)==list2[len(list2)-1]:
-               list2.append(self[index].co2_to(city))
-            else:
-               list2[len(list)-1]+=self[index].co2_to(city)
+        for i in range (len(list1)):
+            count=0.0
+            for index in self.cities:
+                if(index.country==list1[i]):
+                    count+=index.co2_to(city)*index.number
+            list2.append(count)
         dict_cbc = dict(zip(list1, list2))
         return dict_cbc
 
     def summary(self, city: City):
+        list1=self.countries()
         x=int(round(self.total_co2(city)/1000,0))
         z=int(round(self.total_distance_travel_to(city),0))
         print("Host city:",city.name,"(",city.country,")")
         print("Total CO2:",x)
-        print("Total attendees travelling to",city.name,"from",len(self)-1,"different cities:",z)
+        print("Total attendees travelling to",city.name,"from",len(list1),"different cities:",z)
 
     def sorted_by_emissions(self) -> List[Tuple[str, float]]:
         raise NotImplementedError
