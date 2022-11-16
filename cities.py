a1e2416b2ff087b9b6357b05acce0248b0eace93
Yuyang Lin <yuyang.lin22@ucl.ac.uk>
2022-11-16 15:14:11 +0000

    distance up2

--- cities.py
+++ cities.py
@@ -22,10 +22,10 @@ class City:
 
     
     def distance_to(self, other: 'City') -> float:
-        x1=math.radian(self.latitude)
-        y1=math.radian(self.longitude)
-        x2=math.radian(other.latitude)
-        y2=math.radian(other.longitude)
+        x1=math.radians(self.latitude)
+        y1=math.radians(self.longitude)
+        x2=math.radians(other.latitude)
+        y2=math.radians(other.longitude)
         d=12742*math.asin(math.sqrt(math.pow(math.sin(0.5*(x1-x2)),2)+math.cos(x1)*math.cos(x2)*math.pow(math.sin(0.5*(y1-y2)),2)))
         return d
 
