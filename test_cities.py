3ac1bfc930d256a249ef65459095d06014fef03e
Yuyang Lin <yuyang.lin22@ucl.ac.uk>
2022-11-16 15:01:51 +0000

    save test

--- test_cities.py
+++ test_cities.py
@@ -6,4 +6,15 @@ import csv
 filepath = Path(r'C:\Users\10747\Desktop\comp233\conference\attendee_locations.csv')
 collection = read_attendees_file(filepath)
 
-print(collection.plot_top_emitters(collection.cities[0],7,True))
+
+def test_distance(self,other):
+    self.distance_to(other)
+
+def test_CityCollection(self,other):
+    print(self)
+
+def test_hostcity(self,other):
+    print(collection.plot_top_emitters(collection.cities[0],7,True))
+
+def test_sorted_by_emissions(self):
+    print(self.sorted_by_emissions())
