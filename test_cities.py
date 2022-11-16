efe123a06c89eb981668473546303fac7f319918
Yuyang Lin <yuyang.lin22@ucl.ac.uk>
2022-11-16 15:36:02 +0000

    test

--- test_cities.py
+++ test_cities.py
@@ -2,19 +2,28 @@ from cities import City, CityCollection
 from pathlib import Path
 from utils import read_attendees_file
 import csv
+import pytest
 
 filepath = Path(r'C:\Users\10747\Desktop\comp233\conference\attendee_locations.csv')
 collection = read_attendees_file(filepath)
 
 
-def test_distance(self,other):
-    self.distance_to(other)
+def test_distance():
+    exp_error_lon_number = "invalid longitude"
+    exp_error_att_number = "invalid attendance number"
+    exp_error_lat_number = "invalid longitude"
+    with pytest.raises(AssertionError, match=exp_error_lon_number):
+        city = City('Zurich', 'Switzerland' , 52, 47.22, 360)
+    with pytest.raises(AssertionError, match=exp_error_att_number):
+        city = City('Zurich', 'Switzerland' , -5, 47.22, 140)
+    with pytest.raises(AssertionError, match=exp_error_lat_number):
+        city = City('Zurich', 'Switzerland' , 52, 100, 50)
 
-def test_CityCollection(self,other):
-    print(self)
+def test_CityCollection():
+    raise NotImplementedError
 
-def test_hostcity(self,other):
-    print(collection.plot_top_emitters(collection.cities[0],7,True))
+def test_hostcity():
+    raise NotImplementedError
 
-def test_sorted_by_emissions(self):
-    print(self.sorted_by_emissions())
+def test_sorted_by_emissions():
+    raise NotImplementedError
